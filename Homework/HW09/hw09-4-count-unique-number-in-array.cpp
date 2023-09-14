dacdd6e6f0b5d01e11cb7315fc5ac26de9b76e50
wachilanee <wachilanee47@gmail.com>
2023-09-15 00:48:58 +0700

    Hw9-4

--- Homework/HW09/hw09-4-count-unique-number-in-array.cpp
+++ Homework/HW09/hw09-4-count-unique-number-in-array.cpp
@@ -47,3 +47,51 @@
         7 -> 1 value.
         9 -> 2 values.
 */
+#include <stdio.h>
+int main() {
+    
+    int N, i, j ;
+
+    printf( "Input N :" );
+    scanf( "%d", &N );
+
+    int num[ N ] ;
+    int unique[ N ] ;
+    int count[ N ] ;
+    int un_c = 0 ;
+    int b_num = 1 ;
+
+    for ( i = 0 ; i < N ; i++){
+        printf( "Element[%d] :\n", i ) ;
+        scanf( "%d", &num[ i ] );
+    }
+
+    for ( i = 0 ; i < N ; i++) {
+        count[ i ] = 0 ;
+    }
+
+    for ( i = 0 ; i < N ; i++) {
+        int bool_num = 1 ;
+        for ( j = 0 ; j < un_c ; j++) {
+            if ( num[ i ] == unique[ j ] ) {
+                count[ j ]++ ;
+                bool_num = 0 ;
+                break;
+            }
+            
+        }
+        if ( bool_num ) {
+            unique[ un_c ] = num[ i ] ;
+            count[ un_c ] = 1 ;
+            un_c++ ;
+        }
+    }
+
+    for ( i = 0 ; i < un_c ; i++) {
+        printf( "%d -> %d value%s.\n", unique[ i ], count[ i ], (count[ i ] == 1) ? "" : "s" );
+    }
+    
+    
+    
+    return 0;
+}
